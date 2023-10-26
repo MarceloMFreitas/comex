f858db924560947f05af4223e111d0e07a1a0b1c
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-10-26 09:28:03 -0300

    Create exibe.php

---
+++ exibe.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'cliente.php';
+
+$cliente = new Cliente('Maria Jose', 'mariajose@gmail.com', '(13)99999-2244', 'Rua 1, N30', 5);
+
+// Exibindo os valores atribuídos à classe
+echo "Nome: " . $cliente->getNome() . "\n";
+echo "E-mail: " . $cliente->getEmail() . "\n";
+echo "Celular: " . $cliente->getCelular() . "\n";
+echo "Endereço: " . $cliente->getEndereco() . "\n";
+echo "Total de Compras: " . $cliente->getTotalCompras() . "\n";
+?>

7ded5c3a07d8c7dc5b725dc77dda9b521b588daa
MarceloMFreitas <143090673+MarceloMFreitas@users.noreply.github.com>
2023-10-26 09:28:12 -0300

    Create manipula_produto.php

---
+++ manipula_produto.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'produto.php';
+
+$produto = new Produto('Notebook', 3000, 5);
+
+echo "Inicial---------------------------------------------------------------\n";
+echo "Nome do Produto: " . $produto->getNome() . "\n";
+echo "Preço do Produto: $" . $produto->getPreco() . "\n";
+echo "Quantidade em Estoque: " . $produto->getQuantidade() . "\n";
+echo "Valor Total em Estoque: $" . $produto->calcularValorTotal() . "\n";
+
+
+$produto->adicionarProduto(2);
+
+echo "Adiciona 2 -----------------------------------------------------------\n";
+echo "Nome do Produto: " . $produto->getNome() . "\n";
+echo "Preço do Produto: $" . $produto->getPreco() . "\n";
+echo "Quantidade em Estoque: " . $produto->getQuantidade() . "\n";
+echo "Valor Total em Estoque: $" . $produto->calcularValorTotal() . "\n";
+
+$produto->removerProduto(1);
+
+echo "Remove 1 --------------------------------------------------------------\n";
+echo "Nome do Produto: " . $produto->getNome() . "\n";
+echo "Preço do Produto: $" . $produto->getPreco() . "\n";
+echo "Quantidade em Estoque: " . $produto->getQuantidade() . "\n";
+echo "Valor Total em Estoque: $" . $produto->calcularValorTotal() . "\n";
+
+?>
